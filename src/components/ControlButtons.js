0ad67bbc5142af8ee72601bb34687274ce67402e
songchez <tama4840@gmail.com>
2025-01-13 00:37:15 +0900

    버튼해결

--- src/components/ControlButtons.js
+++ src/components/ControlButtons.js
@@ -20,24 +20,31 @@ function ControlButtons({
           >
             <FaPlay /> {isPaused ? "다시 시작" : "시작"}
           </button>
-
-          <button
-            onClick={onPause}
-            className="btn btn-warning flex items-center gap-2"
-            disabled={!isRunning || isPaused}
-          >
-            <FaPause /> 일시정지
-          </button>
+          {isRunning ? (
+            <button
+              onClick={onPause}
+              className="btn btn-warning flex items-center gap-2"
+              disabled={!isRunning || isPaused}
+            >
+              <FaPause /> 일시정지
+            </button>
+          ) : (
+            <></>
+          )}
         </>
       )}
-      <button
-        onClick={onStop}
-        className={`btn ${
-          isCompleted ? "btn-success" : "btn-error"
-        } flex items-center gap-2`}
-      >
-        <FaStop /> {isCompleted ? "처음으로" : "중지"}
-      </button>
+      {isRunning || isCompleted ? (
+        <button
+          onClick={onStop}
+          className={`btn ${
+            isCompleted ? "btn-success" : "btn-error"
+          } flex items-center gap-2`}
+        >
+          <FaStop /> {isCompleted ? "처음으로" : "중지"}
+        </button>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
